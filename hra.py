d1a79d977ae50e4e6fbef8aa4673e8c5b5c9e45a
Filakcz <73638102+Filakcz@users.noreply.github.com>
2025-05-18 21:52:57 +0200

    Banany 🍌 (energie)

--- hra.py
+++ hra.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 
 pygame.init()
 
@@ -15,10 +16,10 @@ krok = 10
 
 def generace_bod(x):
     i = x / 10
-    obtiznost = 1 + (x / 25000)
+    obtiznost = 1 + (x / obtiznost_mapy)
     y = (obrazovka_vyska * 0.7
-         + math.sin(i * 0.004 * obtiznost) * (120 * obtiznost)
-         + math.sin(i * 0.025 * obtiznost + math.cos(i * 0.002)) * (60 * obtiznost)
+         + math.sin(i * 0.004) * (120 * obtiznost)
+         + math.sin(i * 0.025 + math.cos(i * 0.002)) * (60 * obtiznost)
          + math.sin(i * 0.13 + math.cos(i * 0.03)) * (18 + obtiznost * 5)
          + math.sin(i * 0.0025)
          + math.cos(i * 0.7) * 2
@@ -59,7 +60,7 @@ class CyklistickeKolo(pygame.sprite.Sprite):
         self.top_rychlost = 18
         self.gravitace = 1.2
         self.odpor_vzduchu = 0.02
-        self.skok = 30
+        self.skok = 25
 
         self.kolo_polomer = 48
         self.kolo_vzdalenost = 140
@@ -77,18 +78,18 @@ class CyklistickeKolo(pygame.sprite.Sprite):
         self.animace_index = 0
 
     def pohyb(self, keys):
-        if keys[pygame.K_a]:
-            self.rychlost_x -= self.akcelerace
-            self.animace_index -= 0.5
-            if self.animace_index < 0:
-                self.animace_index = self.animace_pocet - 1
-        if keys[pygame.K_d]:
-            self.rychlost_x += self.akcelerace
-            self.animace_index += 0.5
-            if self.animace_index >= self.animace_pocet:
-                self.animace_index = 0
-        if keys[pygame.K_w]:
-            if self.je_na_zemi():
+        if self.je_na_zemi():
+            if keys[pygame.K_a]:
+                self.rychlost_x -= self.akcelerace
+                self.animace_index -= 0.5
+                if self.animace_index < 0:
+                    self.animace_index = self.animace_pocet - 1
+            if keys[pygame.K_d]:
+                self.rychlost_x += self.akcelerace
+                self.animace_index += 0.5
+                if self.animace_index >= self.animace_pocet:
+                    self.animace_index = 0
+            if keys[pygame.K_w]:
                 self.rychlost_y = -self.skok
         if keys[pygame.K_s]:
             print("Zadni kolo x:", self.x_zadni_kolo)
@@ -103,7 +104,7 @@ class CyklistickeKolo(pygame.sprite.Sprite):
     def aktualizace(self):
         self.rychlost_y += self.gravitace
 
-        if not self.je_na_zemi(30):
+        if not self.je_na_zemi(50):
             self.y_zadni_kolo += self.rychlost_y
             self.y_predni_kolo = self.y_zadni_kolo
         else:
@@ -158,6 +159,32 @@ class CyklistickeKolo(pygame.sprite.Sprite):
 
         blitRotate(screen, ram, (self.x_zadni_kolo - kamera_x, self.y_zadni_kolo - self.kolo_polomer - kamera_y), (0,ram.get_height()), uhel_ramu)
 
+    def hitbox(self):
+        vyska_ramu = self.ram_obrazky[0].get_height()
+
+        return pygame.Rect(self.x_zadni_kolo, max(self.y_zadni_kolo, self.y_predni_kolo) - vyska_ramu, self.x_predni_kolo - self.x_zadni_kolo, vyska_ramu)
+
+
+class Banan(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.svet_x = x
+        self.svet_y = y
+        self.image = banan_img
+
+    def vykresli(self, screen, kamera_x, kamera_y):
+        screen.blit(self.image, (self.svet_x - kamera_x, self.svet_y - kamera_y))
+    
+
+def vykresli_ui(screen, km, energie):
+    text_km = font.render(f"Ujeto: {round(km/1000,1)} km", True, (0, 0, 0))
+    screen.blit(text_km, (22, 20))
+
+    pygame.draw.rect(screen, (50, 50, 50), (20, 90, 250, 40))
+    if energie > 0:  
+        pygame.draw.rect(screen, (255, 215, 0), (20, 90, 2.5 * energie, 40))  
+    pygame.draw.rect(screen, (0, 0, 0), (20, 90, 250, 40), 2)  
+
 
 def vykresli_teren(screen, kamera_x, kamera_y):
     body = [[0, obrazovka_vyska]]
@@ -172,6 +199,17 @@ def vykresli_teren(screen, kamera_x, kamera_y):
     pygame.draw.polygon(screen, barva_trava, body)
 
 
+font = pygame.font.SysFont("Arial", 50)
+banany = pygame.sprite.Group()
+banan_img = pygame.image.load("img/banan.png").convert_alpha()
+banan_img = pygame.transform.scale(banan_img, (100, 100))
+
+energie = 100
+obtiznost_mapy = 25000 # vyssi = tezsi
+ztrata_energie = 0.05
+pridavek_energie = 30
+rust_vzdalenosti_bananu = 1.5
+
 def main():
     bezi = True
     clock = pygame.time.Clock()
@@ -180,6 +218,13 @@ def main():
 
     kolo = CyklistickeKolo(0, generace_bod(0), ram_obrazky, obrazek_rafku)
 
+    km_ujet = 0
+    energie = 100
+    vzdalenost_bananu = 1000
+
+
+    banany.add(Banan(vzdalenost_bananu, generace_bod(vzdalenost_bananu)-random.randint(100,500)))
+
     while bezi:
         screen.fill(barva_nebe)
         for event in pygame.event.get():
@@ -187,7 +232,8 @@ def main():
                 bezi = False
 
         keys = pygame.key.get_pressed()
-        kolo.pohyb(keys)
+        if energie > 0:
+            kolo.pohyb(keys)
         kolo.aktualizace()
 
         kamera_x = kolo.x - obrazovka_sirka // 2
@@ -196,10 +242,32 @@ def main():
         vykresli_teren(screen, kamera_x, kamera_y)
         kolo.vykresli(kamera_x, kamera_y, screen)
 
+        for banan in banany.copy():
+            banan.vykresli(screen, kamera_x, kamera_y)
+            hitbox = kolo.hitbox()
+            banan_rect = pygame.Rect(banan.svet_x, banan.svet_y, banan.image.get_width(), banan.image.get_height())
+
+            if hitbox.colliderect(banan_rect):
+                energie = min(energie + pridavek_energie, 100)
+                banany.remove(banan)
+
+                vzdalenost_bananu *= rust_vzdalenosti_bananu
+                banany.add(Banan(kolo.x + vzdalenost_bananu, generace_bod(kolo.x + vzdalenost_bananu) - random.randint(100, 500)))
+
+        if kolo.x > vzdalenost_bananu:
+            vzdalenost_bananu *= rust_vzdalenosti_bananu
+            banany.add(Banan(kolo.x + vzdalenost_bananu, generace_bod(kolo.x + vzdalenost_bananu) - random.randint(100, 500)))
+
+        km_ujet = kolo.x
+
+        energie -= ztrata_energie
+        if energie < -10:
+            # hrac muze na setrvacnost ziskat energii :) - par sekund
+            bezi = False
+
+        vykresli_ui(screen, km_ujet, energie)
+
         pygame.display.flip()
         clock.tick(60)
-
-    pygame.quit()
-
 
 main()
